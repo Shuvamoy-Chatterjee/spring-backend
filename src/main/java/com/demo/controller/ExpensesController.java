6bdf35abeb4a87823a6c2d237ed7d40889c7d79f
ShuvamoyChatterjee <suvo1050.sc@gmail.com>
2021-10-14 03:49:30 +0530

    second commit

--- src/main/java/com/demo/controller/ExpensesController.java
+++ src/main/java/com/demo/controller/ExpensesController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.demo.exception.ResourceNotFoundException;
 import com.demo.model.Expenses;
 import com.demo.repository.ExpensesRepository;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://ng-frontend.azurewebsites.net/")
 @RestController
 @RequestMapping("/api/")
 public class ExpensesController {
